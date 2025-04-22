33d788373d2b00290bb6aa6e9edd4c9e80301e68
IT22309310 <it22309310@my.sliit.lk>
2025-04-22 23:14:45 +0530

    test

--- OrderManagement/DTO/CreateOrderDTO.cs
+++ OrderManagement/DTO/CreateOrderDTO.cs
@@ -2,7 +2,7 @@ namespace OrderManagement.DTO
 {
     public class CreateOrderDTO
     {
-        public string CustomerId { get; set; }
+        public string CustomerId { get; set; } //test customer id
         public string RestaurantId { get; set; }
         public List<OrderItemDTO> Items { get; set; }
         public string DeliveryAddress { get; set; }
