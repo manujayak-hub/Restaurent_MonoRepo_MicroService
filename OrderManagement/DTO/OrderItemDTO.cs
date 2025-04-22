63b3428d3bf4d62aafb438a3aa57fbd18a99e6bd
DinidiGallage <dinidisg@gmail.com>
2025-04-22 23:26:43 +0530

    Fix: Oder DTO

--- OrderManagement/DTO/OrderItemDTO.cs
+++ OrderManagement/DTO/OrderItemDTO.cs
@@ -3,7 +3,7 @@ namespace OrderManagement.DTO
     public class OrderItemDTO
     {
         public string ItemId { get; set; }
-        public string Name { get; set; } // optional, for display
+        public string Name { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
     }
