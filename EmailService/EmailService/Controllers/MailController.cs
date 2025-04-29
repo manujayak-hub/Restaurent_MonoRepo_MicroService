184b98f063ff309d2debc881f8d5005e50cbd946
manujaya <152595781+manujayak-hub@users.noreply.github.com>
2025-04-29 11:23:32 +0530

    Email Service

---
+++ EmailService/EmailService/Controllers/MailController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmailService.Service;
+
+namespace EmailService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly MailService _mailService;
+
+        public EmailController(MailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        [HttpPost("send")]
+        public IActionResult SendEmail([FromBody] EmailRequest request)
+        {
+            _mailService.SendEmail(request.ToEmail, request.Subject, request.Body);
+            return Ok("Email sent successfully!");
+        }
+    }
+
+    public class EmailRequest
+    {
+        public string ToEmail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ EmailService/EmailService/Service/Mailservice.cs
@@ -0,0 +1,40 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+
+namespace EmailService.Service
+{
+    public class MailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public MailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void SendEmail(string toEmail, string subject, string body)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Foodies", "info@Foodies.com"));
+            message.To.Add(new MailboxAddress("", toEmail));
+            message.Subject = subject;
+
+            message.Body = new TextPart("plain")
+            {
+                Text = body
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, false);
+
+                // Authenticate if needed
+                client.Authenticate("s1.se.we.35@gmail.com", "cknj tkuc sujj jrgw");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
